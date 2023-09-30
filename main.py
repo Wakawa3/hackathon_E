b91c16016a52af443dee5e474263d64ca273e9d2
Ryo-Kawakami <s23003@st.kic.ac.jp>
2023-09-30 17:02:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 #test2
 #test3
 #test4
+#内容:複数の座標のうちから、座標をランダムに選んで、座標を返す関数
+
+import random
+
+def creat_dots():
+
+    x_dots=[100,200,300,400]
+    random_x = random.choice(x_dots)
+
+    y_dots=[100,200,300,400]
+    random_y = random.choice(y_dots)
+
+    print(random_x)
+    print(random_y)    
+    return (random_x,random_y)
+
+sample1=creat_dots()
+print(sample1)
