397d49112fd65691919b42c315e28a3dec165a7f
Wakawa3 <70763641+Wakawa3@users.noreply.github.com>
2023-09-30 17:20:40 +0900

    update main

--- main.py
+++ main.py
@@ -18,5 +18,30 @@ def creat_dots():
     print(random_y)    
     return (random_x,random_y)
 
-sample1=creat_dots()
-print(sample1)
+class FoodInfo:
+    def __init__(self):
+        self.name = ""
+        # self.type = ""
+        # self.vertical = ""
+        # self.horizontal = ""
+        self.pos = (0.0, 0.0)
+
+if __name__ == "__main__":
+    type_list = ["staple", "main", "side"]
+    random.sample(type_list, 3)
+    staple_list = ["ご飯", "パスタ"]
+    main_list = ["唐揚げ", "ハンバーグ"]
+    side_list = ["もやし炒め", "ほうれん草のおひたし"]
+    food_list = []
+    for type in type_list:
+        food_list.append(FoodInfo())
+        if(type == "staple"):
+            food_list[-1].name = random.choice(staple_list)
+            food_list[-1].pos = creat_dots() #仮
+        if(type == "main"):
+            food_list[-1].name = random.choice(main_list)
+            food_list[-1].pos = creat_dots() #仮
+        if(type == "side"):
+            food_list[-1].name = random.choice(side_list)
+            food_list[-1].pos = creat_dots() #仮
+    #draw(food_list)
