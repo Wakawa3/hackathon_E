91efc5a52e8d15285f033c8c9abb6f844607b388
Wakawa3 <70763641+Wakawa3@users.noreply.github.com>
2023-10-01 11:38:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,40 +27,6 @@ def check_dup(pos: tuple[int,int], food_list: list[FoodInfo]):
 
 
 if __name__ == "__main__":
-    type_list = ["staple", "main", "side", "side"]
-    random.sample(type_list, 4)
-    staple_list = ["ご飯", "パスタ"]
-    main_list = ["唐揚げ", "ハンバーグ", "にくじゃが", "コロッケ"]
-    side_list = ["もやし炒め", "ほうれん草のおひたし"]
-    food_list = []
-    for type in type_list:
-        food_list.append(FoodInfo())
-        if(type == "staple"):
-            food_list[-1].name = random.choice(staple_list)
-            pos = creat_dots()
-            while check_dup(pos, food_list) == False:
-                pos = creat_dots()
-            food_list[-1].pos = pos
-
-        if(type == "main"):
-            food_list[-1].name = random.choice(main_list)
-            pos = creat_dots()
-            while check_dup(pos, food_list) == False:
-                pos = creat_dots()
-            food_list[-1].pos = pos
-
-        if(type == "side"):
-            food_list[-1].name = random.choice(side_list)
-            pos = creat_dots()
-            while check_dup(pos, food_list) == False:
-                pos = creat_dots()
-            food_list[-1].pos = pos
-    
-    for i in food_list:
-        print(i.name)
-        print(i.pos)
-    
-
     root = tk.Tk()
     root.geometry("300x300")
 
@@ -70,15 +36,49 @@ if __name__ == "__main__":
     # Canvasを配置
     canvas.pack(fill = tk.BOTH, expand = True)
 
+    def main_loop():
+        type_list = ["staple", "main", "side", "side"]
+        random.sample(type_list, 4)
+        staple_list = ["ご飯", "パスタ"]
+        main_list = ["唐揚げ", "ハンバーグ", "にくじゃが", "コロッケ"]
+        side_list = ["もやし炒め", "ほうれん草のおひたし"]
+        food_list = []
+        for type in type_list:
+            food_list.append(FoodInfo())
+            if(type == "staple"):
+                food_list[-1].name = random.choice(staple_list)
+                pos = creat_dots()
+                while check_dup(pos, food_list) == False:
+                    pos = creat_dots()
+                food_list[-1].pos = pos
+
+            if(type == "main"):
+                food_list[-1].name = random.choice(main_list)
+                pos = creat_dots()
+                while check_dup(pos, food_list) == False:
+                    pos = creat_dots()
+                food_list[-1].pos = pos
+
+            if(type == "side"):
+                food_list[-1].name = random.choice(side_list)
+                pos = creat_dots()
+                while check_dup(pos, food_list) == False:
+                    pos = creat_dots()
+                food_list[-1].pos = pos
+
+            for food in food_list:
+                canvas.create_rectangle(food.pos.index(0)-60,food.pos.index(1)+45,food.pos.index(0)+60,food.pos.index(1)-45, fill = "white", width = 5)
+                canvas.create_text(food.pos.index(0),food.pos.index(1), text=food.name, anchor="se", font=("HG丸ｺﾞｼｯｸM-PRO",15))
+        
+        # for i in food_list:
+        #     print(i.name)
+        #     print(i.pos)
+        
+
+
     # 図形の描画 
-    def hyozi(Food_list:list[FoodInfo]):
-        for food in Food_list:
-            canvas.create_rectangle(food.pos.index(0)-60,food.pos.index(1)+45,food.pos.index(0)+60,food.pos.index(1)-45, fill = "white", width = 5)
-            canvas.create_text(food.pos.index(0),food.pos.index(1), text=food.name, anchor="se", font=("HG丸ｺﾞｼｯｸM-PRO",15))
-    
 
-
-    button=tk.Button(root,text="push",command = hyozi(food_list))
+    button=tk.Button(root,text="push",command = main_loop)
     button.pack()
 
     tk.mainloop()
