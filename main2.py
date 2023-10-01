5e936e5617384111b2e94bcce25c765d091bc349
Wakawa3 <70763641+Wakawa3@users.noreply.github.com>
2023-10-01 14:16:34 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -39,7 +39,7 @@ def check_dup(pos: tuple[int,int], food_list: list[FoodInfo]):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("800x600")
+    root.geometry("520x420")
 
 
     # Canvasの作成
@@ -53,36 +53,26 @@ if __name__ == "__main__":
         random.sample(type_list, 4)
         staple_list = [FoodBasicInfo("ご飯","主食",200,300), FoodBasicInfo("パスタ","主食",200,300)]
         main_list = [FoodBasicInfo("唐揚げ","主菜",200,300), FoodBasicInfo("ハンバーグ","主菜",200,300), FoodBasicInfo("にくじゃが","主菜",200,300), FoodBasicInfo("コロッケ","主菜",200,300)]
-        side_list = [FoodBasicInfo("もやし炒め","副菜",100,150), FoodBasicInfo("ほうれん草のおひたし","副菜",100,200)]
+        side_list = [FoodBasicInfo("もやし炒め","副菜",100,150), FoodBasicInfo("ほうれん草のおひたし","副菜",100,200), FoodBasicInfo("煮物","副菜",70,200)]
         food_list = []
         i = 0
         for type in type_list:
+            food:FoodBasicInfo
             if(type == "staple"):
                 food = random.choice(staple_list)
-                # pos = creat_dots()
-                pos = decide_dots.decide_dots(food_list,800,600,food.horizontal, food.vertical)
-                food_list.append(FoodInfo(food))
-                # while check_dup(pos, food_list) == False:
-                #     pos = creat_dots()
-                food_list[-1].pos = pos
 
             if(type == "main"):
                 food = random.choice(main_list)
-                # pos = creat_dots()
-                pos = decide_dots.decide_dots(food_list,800,600,food.horizontal, food.vertical)
-                food_list.append(FoodInfo(food))
-                # while check_dup(pos, food_list) == False:
-                #     pos = creat_dots()
-                food_list[-1].pos = pos
 
             if(type == "side"):
                 food = random.choice(side_list)
-                # pos = creat_dots()
-                pos = decide_dots.decide_dots(food_list,800,600,food.horizontal, food.vertical)
-                food_list.append(FoodInfo(food))
-                # while check_dup(pos, food_list) == False:
-                #     pos = creat_dots()
-                food_list[-1].pos = pos
+            
+            # pos = creat_dots()
+            pos = decide_dots.decide_dots(food_list,800,420,food.horizontal, food.vertical)
+            food_list.append(FoodInfo(food))
+            # while check_dup(pos, food_list) == False:
+            #     pos = creat_dots()
+            food_list[-1].pos = pos
 
 
         for food in food_list:
