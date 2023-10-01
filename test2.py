873aed8c68e5d0e1aefc8717c3c66da937fb9b2c
yuta <yutaharry0620@gmail.com>
2023-10-01 11:24:05 +0900

    test2

---
+++ test2.py
@@ -0,0 +1,96 @@
+#test
+#test2
+#test3
+#test4
+#内容:複数の座標のうちから、座標をランダムに選んで、座標を返す関数
+
+import random
+
+class FoodInfo:
+    def __init__(self):
+        self.name = ""
+        # self.type = ""
+        # self.vertical = ""
+        # self.horizontal = ""
+        self.pos = (0.0, 0.0)
+
+def creat_dots():
+
+    x_dots=[120,240]
+    random_x = random.choice(x_dots)
+
+    y_dots=[70,160]
+    random_y = random.choice(y_dots)
+
+    # print(random_x)
+    # print(random_y)    
+    return (random_x,random_y)
+
+def check_dup(pos: tuple[int,int], food_list: list[FoodInfo]):
+    for food in food_list:
+        if food.pos == pos:
+            return False
+    return True
+
+if __name__ == "__main__":
+    type_list = ["staple", "main", "side", "side"]
+    random.sample(type_list, 4)
+    staple_list = ["ご飯", "パスタ"]
+    main_list = ["唐揚げ", "ハンバーグ", "にくじゃが", "コロッケ"]
+    side_list = ["もやし炒め", "ほうれん草のおひたし"]
+    food_list = []
+    for type in type_list:
+        food_list.append(FoodInfo())
+        if(type == "staple"):
+            food_list[-1].name = random.choice(staple_list)
+            pos = creat_dots()
+            while check_dup(pos, food_list) == False:
+                pos = creat_dots()
+            food_list[-1].pos = pos
+
+        if(type == "main"):
+            food_list[-1].name = random.choice(main_list)
+            pos = creat_dots()
+            while check_dup(pos, food_list) == False:
+                pos = creat_dots()
+            food_list[-1].pos = pos
+
+        if(type == "side"):
+            food_list[-1].name = random.choice(side_list)
+            pos = creat_dots()
+            while check_dup(pos, food_list) == False:
+                pos = creat_dots()
+            food_list[-1].pos = pos
+
+    #draw(food_list) #ここで描画
+    
+    for i in food_list:
+        print(i.name)
+        print(i.pos)
+
+
+import tkinter as tk
+import random
+
+root = tk.Tk()
+root.geometry("300x300")
+
+
+# Canvasの作成
+canvas = tk.Canvas(root, bg = "white")
+# Canvasを配置
+canvas.pack(fill = tk.BOTH, expand = True)
+
+data = FoodInfo()
+
+# 図形の描画 
+def hyozi(Food_list:list[FoodInfo]):
+    for food in Food_list:
+        canvas.create_rectangle(food.pos.index(0)-60,food.pos.index(1)+45,food.pos.index(0)+60,food.pos.index(1)-45, fill = "white", width = 5)
+        canvas.create_text(food.pos.index(0),food.pos.index(1), text=food.name, anchor="se", font=("HG丸ｺﾞｼｯｸM-PRO",15))
+   
+
+button=tk.Button(root,text="push",command = hyozi)
+button.pack()
+
+tk.mainloop()
