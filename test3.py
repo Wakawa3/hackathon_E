039859a6da92b9569735dd3c74f5dccb7cab2991
Wakawa3 <70763641+Wakawa3@users.noreply.github.com>
2023-10-01 11:19:15 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,5 +1,5 @@
 import random
-
+import main2
 
 #縦横の長さ1組を引数、例(100,100)
 # 計算した座標を返す
@@ -8,8 +8,13 @@ def decide_dots(bento_width, bento_height,
                 min_width, 
                 max_width, 
                 min_height, 
-                max_height):
+                max_height,
+                food_list: list[main2.FoodInfo]):
     
+    # food_list[0].pos 0番目の品の中心座標を取得
+    # food_list[0].horizontal 0番目の品の横の長さが取得
+    # food_list[0].vertical 0番目の品の縦の長さが取得
+
     box =[]
     for a in range(number):
         x_width = random.randint(min_width, max_width)
@@ -19,6 +24,7 @@ def decide_dots(bento_width, bento_height,
 
         box.append((x,y,x_width,y_height))
 
-    return box
+    return box #新しく置く箱の中心座標
+    # return (x_pos, y_pos) タプルの形で返す
 
 
