90f0d7688cbb0d4b848efc24af7adc3c3dc363e4
Ryo-Kawakami <s23003@st.kic.ac.jp>
2023-10-01 11:11:37 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,32 +1,24 @@
 import random
 
-def decide_dots(container_width, container_height, 
+
+#縦横の長さ1組を引数、例(100,100)
+# 計算した座標を返す
+def decide_dots(bento_width, bento_height, 
                 number, 
-                min_rect_width, 
-                max_rect_width, 
-                min_rect_height, 
-                max_rect_height):
+                min_width, 
+                max_width, 
+                min_height, 
+                max_height):
     
     box =[]
     for a in range(number):
-        width = random.randint(min_rect_width, max_rect_width)
-        height = random.randint(min_rect_height,max_rect_height)
-        x = random.randint(0,container_width - width)
-        y = random.randint(0,container_height - height)
+        x_width = random.randint(min_width, max_width)
+        y_height = random.randint(min_height,max_height)
+        x = random.randint(0,bento_width - x_width)
+        y = random.randint(0,bento_height - y_height)
 
-        box.append((x,y,width,height))
+        box.append((x,y,x_width,y_height))
 
     return box
 
-container_width = 200
-container_height = 200
-num_rectangles = 5
-min_rect_width = 10
-max_rect_width = 50
-min_rect_height = 10
-max_rect_height = 50
 
-rectangles = decide_dots(container_width, container_height, num_rectangles, min_rect_width, max_rect_width, min_rect_height, max_rect_height)
-
-for i, rect in enumerate(rectangles):
-    print(f"Rectangle {i+1}: {rect}")
